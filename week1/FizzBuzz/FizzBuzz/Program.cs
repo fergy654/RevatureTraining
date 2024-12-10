7cd8ff3b92450a2d8a4676e029214dbd95cdb8ef
Patrick Ferguson <pfergie2016@gmail.com>
2024-12-10 10:09:25 -0600

    New programs created, FizzBuzz and Magic ball

---
+++ week1/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    FizzBuzz, we have an integer, for every integer that is less than n, we have a few rules:
+    Print "FizzBuzz" if i / 3 and i / 5
+    Print "Fizz" if i / 3
+    Print "Buzz" if i / 5
+    Print i if none of the above are true
+*/
+
+/*Access Modifiers
+
+public - it's the most open access modifier, all classes in the project can access it
+private - most closed, only available to the class itself
+internal - it's limited to a class and it's member, but not static member
+protected - only the class it's children/subclasses
+
+
+static keyword - means we don't have to have an object/instance to 
+call method or access static member, and static member value are shared
+
+*/
+
+    int number = 20;
+    int counter = 1;
+
+    while(counter <= number) {
+        if((counter % 3 == 0) && (counter % 5 == 0)) {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if(counter % 3 == 0) {
+            Console.WriteLine("Fizz");
+        }
+        else if(counter % 5 == 0) {
+            Console.WriteLine("Buzz");
+        }
+        else{
+            Console.WriteLine(counter);
+        }
+
+        counter++;
+    }
---
+++ week1/Magic Ball/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Magic_Ball;
+
+//Magic Ball
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Magic Ball!");
+        Console.WriteLine("Think about your question, and type something:");
+
+        string? userInput = Console.ReadLine(); //? means it can be null
+
+        if(!string.IsNullOrEmpty(userInput)) {
+            Console.WriteLine("\nThe magic ball says:");
+            string prediction = GetPrediction();
+            Console.WriteLine(prediction);
+        }
+        else {
+            Console.WriteLine("Type something!\n");
+            Main(args);
+        }
+    }
+
+
+
+    public static string GetPrediction() {
+        //This is how we mark our variable of a stirng array data type
+        string[] predicitons = {
+            "It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy, try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Don't count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful"
+        };
+
+        //Generate a random number using Random built-in class
+        Random myRandomNumber = new Random();
+        int randomIndex = myRandomNumber.Next(0, predicitons.Length);
+
+        return predicitons[randomIndex];
+
+    }
+}
